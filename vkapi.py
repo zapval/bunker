9620ec4dc9bb3ce83bad90f116f08bb451341633
zapval <zapvalgen2000@gmail.com>
2021-10-23 20:00:49 +0300

    изменил токены, бесполезный коммит

--- vkapi.py
+++ vkapi.py
@@ -11,4 +11,4 @@ session = vk.Session()
 api = vk.API(session, v = 5.103)
 
 def send_message(peer_id, random_id, keyboard, token, message, attachment):
-    api.messages.send(access_token = "d464a412ae97183736e77e0657c1827c342fee3635cf733bfa0333adbde63b16c5f4212abedde24fd26c5", keyboard = keyboard, random_id = random.randint(1,100), peer_id=str(peer_id), message=message, attachment=attachment)
+    api.messages.send(access_token = "0421951555eb8b3ae1cf323657ebf025d9886064857bd9e411df8aa5050df7fd5f67837bed3f614dbd200", keyboard = keyboard, random_id = random.randint(1,100), peer_id=str(peer_id), message=message, attachment=attachment)
